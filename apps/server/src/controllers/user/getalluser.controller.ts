78a9c994bada36b915bb95e54dbf21e8fccd35cc
Pankaj-krCP <pankajkumar70792@gmail.com>
2024-04-14 17:43:20 +0530

    minor

--- apps/server/src/controllers/user/getalluser.controller.ts
+++ apps/server/src/controllers/user/getalluser.controller.ts
@@ -5,7 +5,9 @@ import userModel from "../../models/user.modal";
 export const getAllUser = catchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const allUser = await userModel.find().select("-refreshToken -password");
+      const allUser: any = await userModel
+        .find()
+        .select("-refreshToken -password");
       res.status(200).json({
         succes: true,
         data: { allUser },
