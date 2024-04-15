bfcbbe794cd65e0098d084647f39ed2abfa61012
Pankaj-krCP <pankajkumar70792@gmail.com>
2024-04-15 20:50:33 +0530

    multer

--- apps/server/src/middlewares/multer.middleware.ts
+++ apps/server/src/middlewares/multer.middleware.ts
@@ -1,16 +1,26 @@
-const multer = require("multer");
-const path = require("path");
+import { Request } from "express";
+import multer from "multer";
+import path from "path";
+
 const storage = multer.diskStorage({
-  destination: function (req: Request, file: any, cb: any) {
+  destination: function (
+    req: Request,
+    file: Express.Multer.File,
+    cb: Function
+  ) {
     cb(null, path.join(__dirname, "../temp/my-upload"));
   },
-  filename: function (req: Request, file: any, cb: any) {
+  filename: function (req: Request, file: Express.Multer.File, cb: Function) {
     const uniquesuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, file.fieldname + "-" + uniquesuffix + ".jpeg");
   },
 });
 
-const multerFilter = (req: Request, file: any, cb: any) => {
+const multerFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: Function
+) => {
   if (file.mimetype.startsWith("image")) {
     cb(null, true);
   } else {
