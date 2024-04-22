829cbf06b5c6390f9dc28f4c4fad7b4c2ee1043d
Pankaj-krCP <pankajkumar70792@gmail.com>
2024-04-22 14:21:51 +0530

    added gradient shadow styles

--- packages/ui/src/header.tsx
+++ packages/ui/src/header.tsx
@@ -2,12 +2,11 @@
 
 import { FC, useState } from "react";
 import { ThemeSwitcher } from "./component/header/ThemeSwitcher";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
-import { ImCancelCircle } from "react-icons/im";
+import { HiOutlineMenuAlt2, HiOutlineMenuAlt3 } from "react-icons/hi";
 import NavItems from "./component/header/NavItems";
 import Logo from "./component/header/Logo";
 import Profile from "./component/header/Profile";
-import { AbsoluteBox } from "./utils/constant";
+import { AbsoluteBox, BoxTopToBottomGradient } from "./utils/constant";
 
 export interface NavItemsProps {
   name: string;
@@ -25,7 +24,7 @@ export const Header: FC<Props> = ({ navItemsData, auth, setAuth }) => {
   return (
     <div className="w-full relative">
       <div
-        className={`border-b border-gray-500 top-0 left-0 w-full h-[70px] z-[80]`}
+        className={`${BoxTopToBottomGradient} border-t-0 border-l-0 border-r-0 top-0 left-0 w-full h-[70px] z-[80] shadow-lg`}
       >
         <div className="w-[98%]">
           <div className="h-[70px] flex items-center justify-between p-3">
@@ -37,22 +36,30 @@ export const Header: FC<Props> = ({ navItemsData, auth, setAuth }) => {
               <ThemeSwitcher />
               <Profile auth={auth} setAuth={setAuth} />
               <div className="800px:hidden">
-                <HiOutlineMenuAlt3
-                  className="cursor-pointer ml-6"
-                  size={24}
-                  onClick={() => setOpenSideBar(true)}
-                />
+                {openSideBar ? (
+                  <HiOutlineMenuAlt2
+                    className="cursor-pointer ml-6"
+                    size={24}
+                    onClick={() => setOpenSideBar(false)}
+                  />
+                ) : (
+                  <HiOutlineMenuAlt3
+                    className="cursor-pointer ml-6"
+                    size={24}
+                    onClick={() => setOpenSideBar(true)}
+                  />
+                )}
                 {openSideBar && (
                   <div
-                    className={`${AbsoluteBox} right-0 top-0 rounded-tl-none rounded-tr-none rounded-br-none`}
+                    className={`${AbsoluteBox} z-[-1] right-0 top-[100%] border-t-0 rounded-tl-none rounded-tr-none rounded-br-none`}
                   >
-                    <div className="flex justify-end w-full border-b border-gray-500 py-5 pr-4">
+                    {/* <div className="flex justify-end w-full border-b border-gray-500 py-5 pr-4">
                       <ImCancelCircle
                         className="cursor-pointer"
                         size={24}
                         onClick={() => setOpenSideBar(false)}
                       />
-                    </div>
+                    </div> */}
                     <NavItems navItemsData={navItemsData} />
                   </div>
                 )}
--- packages/ui/src/utils/constant.ts
+++ packages/ui/src/utils/constant.ts
@@ -1,4 +1,13 @@
+// export const ButtonHover =
+//   "hover:bg-gray-200 hover:dark:bg-zinc-600 hover:text-[crimson] hover:dark:text-[#37a39a]";
+// export const AbsoluteBox =
+//   "absolute rounded-lg border border-gray-300 dark:border-gray-600 bg-zinc-100 dark:bg-zinc-700 w-[50%] 800px:w-[25%] p-1";
+
 export const ButtonHover =
-  "hover:bg-gray-200 hover:dark:bg-zinc-600 hover:text-[crimson] hover:dark:text-[#37a39a]";
+  "hover:bg-gradient-to-r hover:from-gray-100 hover:via-gray-200 hover:to-gray-300 hover:dark:from-zinc-500 hover:dark:via-zinc-600 hover:dark:to-zinc-700 hover:text-[crimson] hover:dark:text-[#37a39a] hover:shadow-lg";
+
 export const AbsoluteBox =
-  "absolute rounded-lg border border-gray-300 dark:border-gray-600 bg-zinc-100 dark:bg-zinc-700 w-[50%] 800px:w-[25%] p-1";
+  "absolute rounded-lg border border-gray-300 dark:border-gray-600 bg-gradient-to-r from-zinc-50 via-zinc-100 to-zinc-200 dark:bg-gradient-to-r dark:from-zinc-700 dark:via-zinc-800 dark:to-zinc-900 w-[50%] 800px:w-[25%] p-1 shadow-lg";
+
+export const BoxTopToBottomGradient =
+  "border border-gray-300 dark:border-gray-600 bg-gradient-to-b from-white via-gray-50 to-gray-100 dark:from-zinc-700 dark:via-zinc-800 dark:to-zinc-900 shadow-lg";
