b2fed1957d25c2e077557ef968ffc98de9faa9dc
Pankaj-krCP <pankajkumar70792@gmail.com>
2024-05-21 22:34:38 +0530

    stopping backend working on frontend

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,23 +1,23 @@
-import "dotenv/config";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import { app } from "./server";
-import express from "express";
-import errorHandler from "./utils/errorHandler.helper";
-import errorMiddleware from "./middlewares/error.middleware";
-import { authRoute, userRoute, courseRoute, thumbnailRoute } from "./routes/v1";
+// import "dotenv/config";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import { app } from "./server";
+// import express from "express";
+// import errorHandler from "./utils/errorHandler.helper";
+// import errorMiddleware from "./middlewares/error.middleware";
+// import { authRoute, userRoute, courseRoute, thumbnailRoute } from "./routes/v1";
 
-app.use(express.json({ limit: "50mb" }));
-app.use(cookieParser());
-app.use(cors({ origin: process.env.ORIGIN }));
+// app.use(express.json({ limit: "50mb" }));
+// app.use(cookieParser());
+// app.use(cors({ origin: process.env.ORIGIN }));
 
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/thumbnail", thumbnailRoute);
-app.use("/api/v1/course", courseRoute);
+// app.use("/api/v1/auth", authRoute);
+// app.use("/api/v1/user", userRoute);
+// app.use("/api/v1/thumbnail", thumbnailRoute);
+// app.use("/api/v1/course", courseRoute);
 
-app.all("*", (req, res, next) => {
-  throw new errorHandler(`Route ${req.originalUrl} not found`, 404);
-});
+// app.all("*", (req, res, next) => {
+//   throw new errorHandler(`Route ${req.originalUrl} not found`, 404);
+// });
 
-app.use(errorMiddleware);
+// app.use(errorMiddleware);
